8033ec925e4d5705e0e17a68deb2cc74faacfe67
Mohamed <30614446+TheDigitalPhoenixX@users.noreply.github.com>
2018-07-05 18:46:12 +0200

    Bluetooth feat. now works
    but only with the first button

--- Android/ArduinoDebuggingApp/app/src/main/java/com/example/mohamedxyz/myapplication/MainActivity.java
+++ Android/ArduinoDebuggingApp/app/src/main/java/com/example/mohamedxyz/myapplication/MainActivity.java
@@ -9,12 +9,28 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+import java.io.InputStream;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     //private static final String TAG = "MainActivity";
-
+    private final String DEVICE_ADDRESS="00:21:13:00:EA:2C";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +53,23 @@ public class MainActivity extends AppCompatActivity {
         String TempSensorsAvailability = "Gyroscope/Accelerometer: " + gyroExists + "/" + AccelExists;
         SensorsAvailability.setText(TempSensorsAvailability);
 
+
+
         SendTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Log.d(TAG, "onCreate: ChangingText");
-                Toast.makeText(MainActivity.this, "Text Sent: " + TextBoxToSend.getText().toString(), Toast.LENGTH_SHORT).show();
+                String string = TextBoxToSend.getText().toString();
+                string.concat("\n");
+                try {
+                    outputStream.write(string.getBytes());
+                    Toast.makeText(MainActivity.this, "Text Sent: " + TextBoxToSend.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
                 if (SendLog.getText().toString().equals("")) {
                     SendLog.setText(TextBoxToSend.getText().toString());
                 } else {
@@ -90,8 +118,80 @@ public class MainActivity extends AppCompatActivity {
         BluetoothButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if(BTinit())
+                {
+                    if(BTconnect())
+                    {
+                        Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
+    }
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
     }
 }
